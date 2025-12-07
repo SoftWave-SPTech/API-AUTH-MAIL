4957c691645bd6d2990d0fe968c049c365489853
Lua ♡ <160372370+lualuaSantosS2@users.noreply.github.com>
2025-12-07 13:30:21 -0300

    Create WebConfig.java

---
+++ src/main/java/com/project/softwave/auth/infrastructure/config/WebConfig.java
@@ -0,0 +1,35 @@
+package com.project.api-auth-mail.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${file.PASTA_DOCUMENTOS_PROCESSOS}")
+    private String pastaDocumentosProcessos;
+
+    @Value("${file.PASTA_DOCUMENTOS_PESSOAIS}")
+    private String pastaDocumentosPessoais;
+
+    @Value("${file.PASTA_FOTOS_PERFIS}")
+    private String pastaFotosPerfis;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        // Acesso para Documentos de Processo
+        registry.addResourceHandler("/ArquivosSistemaUsuarios/DocumentosProcessos/**")
+                .addResourceLocations("file:" + pastaDocumentosProcessos + "/");
+
+        // Acesso para Documentos Pessoais
+        registry.addResourceHandler("/ArquivosSistemaUsuarios/DocumentosPessoais/**")
+                .addResourceLocations("file:" + pastaDocumentosPessoais + "/");
+
+        // Acesso para Fotos de Perfil
+        registry.addResourceHandler("/ArquivosSistemaUsuarios/FotosPerfis/**")
+                .addResourceLocations("file:" + pastaFotosPerfis + "/");
+    }
+}
